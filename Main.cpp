aeacb081848d9afbab4b4439a725f2fcc81f9722
yama-can <yama.can95@gmail.com>
2023-07-11 19:26:32 +0900

    [add] includePath機能の追加 [branchname]

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<string> includePaths;
+
 string getFile(ifstream &ifs)
 {
 	string res = "";
@@ -13,11 +15,13 @@ string getFile(ifstream &ifs)
 	return res;
 }
 
-string slove(ifstream &ifs, size_t loop = 0)
+string slove(string currentFile, size_t loop = 0)
 {
+	ifstream ifs(currentFile);
 	if (loop > 100)
 		exit(10);
 	string res = "";
+	int row = 1;
 	while (!ifs.eof())
 	{
 		string line;
@@ -56,7 +60,32 @@ string slove(ifstream &ifs, size_t loop = 0)
 			}
 			if (isFile)
 			{
-				ifstream load(file);
+				string load;
+				if (filesystem::is_regular_file(file))
+				{
+					load = file;
+				}
+				else
+				{
+					bool accepted = false;
+					for (string path : includePaths)
+					{
+						filesystem::path include(path);
+						include.append(file);
+						if (filesystem::is_regular_file(include))
+						{
+							load = include;
+							accepted = true;
+							break;
+						}
+					}
+					if (!accepted)
+					{
+						cerr << "Error: Cannnot Find File: " << file << endl;
+						cerr << "At " << currentFile << ":" << row << endl;
+						exit(11);
+					}
+				}
 				res += slove(load, loop + 1);
 			}
 			else
@@ -65,19 +94,27 @@ string slove(ifstream &ifs, size_t loop = 0)
 			}
 			res += '\n';
 		}
+		row++;
 	}
 	return res;
 }
 
 int main(int args, char **argv)
 {
-	if (args != 3)
+	test();
+	if (args < 3)
 	{
 		cout << "ファイルを入力してください。";
 		return 1;
 	}
-	ifstream ifs(argv[1]);
-	string s = slove(ifs);
+	if (args != 3)
+	{
+		for (int i = 3; i < args; i++)
+		{
+			includePaths.push_back(string(argv[i]));
+		}
+	}
+	string s = slove(argv[1]);
 	ofstream ofs(argv[2]);
 	ofs << s;
 }
